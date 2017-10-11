58dd3bf25c3755a757d0fe8cdb81867a047ff006
Thijs de Haan <thijsdehaan71@hotmail.com>
2017-10-11 18:28:12 +0200

    MainActivity now opens map

--- android/app/src/main/java/nl/pocketquest/pocketquest/MainActivity.kt
+++ android/app/src/main/java/nl/pocketquest/pocketquest/MainActivity.kt
@@ -1,12 +1,51 @@
 package nl.pocketquest.pocketquest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.mapbox_key))
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
     }
+
+    public override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
 }
