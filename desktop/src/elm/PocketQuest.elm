8c215db980c3e111089f7e1a84e1fb4eb515f6cd
Jari Germeraad <jarinoes@gmail.com>
2017-10-18 15:45:07 +0200

    ADD: Inventory overlay

--- desktop/src/elm/PocketQuest.elm
+++ desktop/src/elm/PocketQuest.elm
@@ -1,14 +1,27 @@
 module PocketQuest exposing (main)
 
 import Html exposing (Html)
+import Html.Attributes as Attributes
+import Material
+import Material.Layout as Layout
+import Material.Options as Options
+import Material.Elevation as Elevation
+import Material.Typography as Typo
 
 
 type alias Model =
-    {}
+    { mdl : Material.Model }
+
+
+type alias OverlayOptions =
+    { id : String
+    , header : String
+    , content : List (Html Msg)
+    }
 
 
 type Msg
-    = Nothing
+    = Mdl (Material.Msg Msg)
 
 
 main : Program Never Model Msg
@@ -23,19 +36,59 @@ main =
 
 init : ( Model, Cmd Msg )
 init =
-    {} ! []
+    { mdl = Material.model } ! []
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Nothing ->
-            init
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
 
 
 view : Model -> Html Msg
 view model =
-    Html.text "Hello, World!"
+    viewInventoryOverlay model
+
+
+viewInventoryOverlay : Model -> Html Msg
+viewInventoryOverlay model =
+    viewOverlay model
+        { id = "inventory-overlay"
+        , header = "Inventory"
+        , content = (viewInventoryOverlayContent model)
+        }
+
+
+viewInventoryOverlayContent : Model -> List (Html Msg)
+viewInventoryOverlayContent model =
+    []
+
+
+viewOverlay : Model -> OverlayOptions -> Html Msg
+viewOverlay model { id, header, content } =
+    Options.div
+        [ Options.id id
+        , Elevation.e4
+        ]
+        [ Options.div
+            [ Options.cs "overlay" ]
+            [ Layout.render Mdl
+                model.mdl
+                [ Layout.fixedHeader
+                , Layout.waterfall False
+                ]
+                { header =
+                    [ Options.styled Html.h5
+                        [ Typo.uppercase ]
+                        [ Html.text header ]
+                    ]
+                , drawer = []
+                , tabs = ( [], [] )
+                , main = content
+                }
+            ]
+        ]
 
 
 subscriptions : Model -> Sub Msg
