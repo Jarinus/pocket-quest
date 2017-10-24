e2627a0e734ffd8aee99c827575ebbb74c5487ee
Jari Germeraad <jarinoes@gmail.com>
2017-10-24 12:01:04 +0200

    ADD: Inventory overlay

--- desktop/src/elm/Inventory.elm
+++ desktop/src/elm/Inventory.elm
@@ -1,28 +1,114 @@
-module Inventory exposing (Model, Msg, init, update, view)
+module Inventory exposing (Model, Msg(NewRowWidth), init, update, view)
 
+import Dict exposing (Dict)
 import Html exposing (Html)
+import Html.Attributes as Attributes
 
 
 type alias Model =
-    {}
+    { items : Dict String Int
+    , rowWidth : Int
+    , itemSize : Int
+    , padding : Int
+    }
 
 
 type Msg
-    = Nothing
+    = NewRowWidth Int
 
 
-init : Model
-init =
-    {}
+init : Int -> Model
+init rowWidth =
+    { items =
+        [ ( "Logs", 10 )
+        , ( "Planks", 3 )
+        , ( "Axe", 1 )
+        , ( "Pickaxe", 1 )
+        , ( "Logs2", 10 )
+        , ( "Planks2", 3 )
+        , ( "Axe2", 1 )
+        , ( "Pickaxe2", 1 )
+        ]
+            |> Dict.fromList
+    , rowWidth = rowWidth
+    , itemSize = 48
+    , padding = 12
+    }
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Nothing ->
-            init
+        NewRowWidth rowWidth ->
+            { model | rowWidth = rowWidth }
 
 
-view : Model -> List (Html Msg)
+calculateItemsPerRow : Int -> Int -> Int -> Int
+calculateItemsPerRow padding width itemSize =
+    (width - 32 + padding) // (itemSize + padding)
+
+
+calculateRowTotalWidth : Int -> Int -> Int -> Int
+calculateRowTotalWidth padding itemSize itemsPerRow =
+    ((padding + itemSize) * itemsPerRow) - padding
+
+
+view : Model -> Html Msg
 view model =
-    []
+    let
+        itemsPerRow =
+            calculateItemsPerRow model.padding model.rowWidth model.itemSize
+
+        rowTotalWidth =
+            calculateRowTotalWidth model.padding model.itemSize itemsPerRow
+
+        renderedItems =
+            model.items
+                |> Dict.toList
+                |> List.map viewItem
+                |> split itemsPerRow
+                |> List.map (viewItemRow rowTotalWidth)
+    in
+        Html.div
+            [ Attributes.id "inventory" ]
+            renderedItems
+
+
+viewItem : ( String, Int ) -> Html Msg
+viewItem ( name, amount ) =
+    Html.div
+        [ Attributes.class "inventory-item-container" ]
+        [ Html.div
+            [ Attributes.class "inventory-item" ]
+            [ Html.div
+                [ Attributes.class "inventory-item-name" ]
+                [ Html.text name ]
+            , Html.div
+                [ Attributes.class "inventory-item-amount" ]
+                [ Html.text (toString amount) ]
+            ]
+        ]
+
+
+viewItemRow : Int -> List (Html Msg) -> Html Msg
+viewItemRow width items =
+    Html.div
+        [ Attributes.class "inventory-row"
+        , Attributes.style
+            [ ( "width", (toString width) ++ "px" ) ]
+        ]
+        items
+
+
+
+-- Util
+
+
+split : Int -> List a -> List (List a)
+split i list =
+    case List.take i list of
+        [] ->
+            []
+
+        listHead ->
+            listHead :: split i (List.drop i list)
--- desktop/src/elm/PocketQuest.elm
+++ desktop/src/elm/PocketQuest.elm
@@ -2,25 +2,60 @@ module PocketQuest exposing (main)
 
 import Dict exposing (Dict)
 import Html exposing (Html)
+import Html.Events as Events
 import Html.Attributes as Attributes
-import Inventory
+import Json.Decode as Decode
 import Material
+import Material.Tabs as Tabs
+import Material.Color as Color
 import Material.Layout as Layout
 import Material.Options as Options
 import Material.Elevation as Elevation
 import Material.Typography as Typo
-import Overlay
+import Mouse exposing (Position)
+import Inventory
 
 
 type alias Model =
-    { overlay : Overlay.Model
+    { inventoryPanel : Panel
+    , inventory : Inventory.Model
     , mdl : Material.Model
     }
 
 
+type alias Panel =
+    { tabIndex : Int
+    , tabs : Dict Int PanelTab
+    , width : Int
+    , minWidth : Int
+    , maxWidth : Int
+    , drag : Maybe Drag
+    }
+
+
+type alias PanelTab =
+    { header : String
+    , content : Html Msg
+    }
+
+
+type alias Drag =
+    { start : Position
+    , current : Position
+    }
+
+
 type Msg
-    = OverlayMsg Overlay.Msg
+    = SelectInventoryPanelTab Panel Int
+    | InventoryMsg Inventory.Msg
+    | OnInventoryPanelResizeStart Position
+    | OnInventoryPanelResize Position
+    | OnInventoryPanelResizeEnd Position
     | Mdl (Material.Msg Msg)
+
+
+
+-- Main
 
 
 main : Program Never Model Msg
@@ -33,43 +68,249 @@ main =
         }
 
 
+
+-- Init
+
+
 init : ( Model, Cmd Msg )
 init =
     let
-        ( overlay, overlayCmd ) =
-            Overlay.init
-                [ { header = "Test"
-                  , content = Html.text "Test"
+        inventoryWidth =
+            360
+
+        inventory =
+            Inventory.init inventoryWidth
+
+        inventoryPanel =
+            { tabIndex = 0
+            , tabs =
+                [ { header = "Inventory"
+                  , content = viewInventory inventory
                   }
                 ]
+                    |> List.indexedMap (,)
+                    |> Dict.fromList
+            , width = inventoryWidth
+            , minWidth = inventoryWidth
+            , maxWidth = 600
+            , drag = Nothing
+            }
     in
-        { overlay = overlay
+        { inventoryPanel = inventoryPanel
+        , inventory = inventory
         , mdl = Material.model
         }
-            ! [ Cmd.map (\msg -> OverlayMsg msg) overlayCmd ]
+            ! []
+
+
+mapInventoryMsg : Inventory.Msg -> Msg
+mapInventoryMsg msg =
+    InventoryMsg msg
+
+
+
+-- Update
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        OverlayMsg msg_ ->
+        SelectInventoryPanelTab panel index ->
+            { model
+                | inventoryPanel =
+                    { panel | tabIndex = index }
+            }
+                ! []
+
+        InventoryMsg msg_ ->
+            { model
+                | inventory =
+                    Inventory.update msg_ model.inventory
+            }
+                ! []
+
+        OnInventoryPanelResizeStart position ->
             let
-                ( overlay, overlayCmd ) =
-                    Overlay.update msg_ model.overlay
+                inventoryPanel =
+                    model.inventoryPanel
             in
-                { model | overlay = overlay }
-                    ! [ Cmd.map (\msg -> OverlayMsg msg) overlayCmd ]
+                { model
+                    | inventoryPanel =
+                        { inventoryPanel
+                            | drag = Just (Drag position position)
+                        }
+                }
+                    ! []
+
+        OnInventoryPanelResize position ->
+            case model.inventoryPanel.drag of
+                Just drag ->
+                    let
+                        inventoryPanel =
+                            model.inventoryPanel
+
+                        inventory =
+                            Inventory.update (Inventory.NewRowWidth newWidth) model.inventory
+
+                        xDifference =
+                            position.x - drag.current.x
+
+                        newWidth =
+                            inventoryPanel.width + xDifference
+                    in
+                        if (newWidth < inventoryPanel.minWidth) || (newWidth > inventoryPanel.maxWidth) then
+                            model ! []
+                        else
+                            { model
+                                | inventoryPanel =
+                                    { inventoryPanel
+                                        | drag =
+                                            Drag drag.start position
+                                                |> Just
+                                        , width = newWidth
+                                        , tabs =
+                                            Dict.update 0
+                                                (\tab ->
+                                                    case tab of
+                                                        Just tab_ ->
+                                                            Just { tab_ | content = viewInventory inventory }
+
+                                                        Nothing ->
+                                                            tab
+                                                )
+                                                inventoryPanel.tabs
+                                    }
+                                , inventory =
+                                    inventory
+                            }
+                                ! []
+
+                Nothing ->
+                    model ! []
+
+        OnInventoryPanelResizeEnd position ->
+            let
+                inventoryPanel =
+                    model.inventoryPanel
+            in
+                { model
+                    | inventoryPanel =
+                        { inventoryPanel
+                            | drag = Nothing
+                        }
+                }
+                    ! []
 
         Mdl msg_ ->
             Material.update Mdl msg_ model
 
 
-view : Model -> Html Msg
-view model =
-    Overlay.view model.overlay
-        |> Html.map (\msg -> OverlayMsg msg)
+
+-- Subscriptions
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    case model.inventoryPanel.drag of
+        Nothing ->
+            Sub.none
+
+        Just _ ->
+            Sub.batch
+                [ Mouse.moves OnInventoryPanelResize
+                , Mouse.ups OnInventoryPanelResizeEnd
+                ]
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view { mdl, inventoryPanel } =
+    viewPanel
+        mdl
+        inventoryPanel
+        (SelectInventoryPanelTab inventoryPanel)
+
+
+viewInventory : Inventory.Model -> Html Msg
+viewInventory inventory =
+    Inventory.view inventory
+        |> Html.map mapInventoryMsg
+
+
+viewPanel : Material.Model -> Panel -> (Int -> Msg) -> Html Msg
+viewPanel mdl panel onSelectTab =
+    if Dict.size panel.tabs > 0 then
+        viewPanel_ mdl panel onSelectTab
+    else
+        Html.text ""
+
+
+viewPanel_ : Material.Model -> Panel -> (Int -> Msg) -> Html Msg
+viewPanel_ mdl panel onSelectTab =
+    let
+        tabProperties =
+            [ Options.cs "overlay overlay-left"
+            , Tabs.onSelectTab onSelectTab
+            , Tabs.activeTab panel.tabIndex
+            ]
+
+        tabLabels =
+            Dict.values panel.tabs
+                |> List.map (\tab -> tab.header)
+                |> List.map viewTabLabel
+
+        tabContent =
+            case Dict.get panel.tabIndex panel.tabs of
+                Just tab ->
+                    [ tab.content ]
+
+                Nothing ->
+                    []
+    in
+        Options.div
+            [ Options.cs "overlay-wrapper"
+            , Attributes.style
+                [ ( "width", (toString panel.width) ++ "px" )
+                , ( "min-width", (toString panel.minWidth) ++ "px" )
+                , ( "max-width", (toString panel.maxWidth) ++ "px" )
+                ]
+                |> Options.attribute
+            ]
+            [ Tabs.render Mdl
+                [ 0 ]
+                mdl
+                tabProperties
+                tabLabels
+                tabContent
+            , viewOverlayResizeHandle panel
+            ]
+
+
+viewTabLabel : String -> Tabs.Label Msg
+viewTabLabel header =
+    Tabs.label
+        [ Options.cs "overlay-tab"
+        , Options.center
+        , Color.background Color.primary
+        , Color.text Color.white
+        ]
+        [ Html.text header ]
+
+
+viewOverlayResizeHandle : Panel -> Html Msg
+viewOverlayResizeHandle panel =
+    Options.div
+        [ Options.css "width" "4px"
+        , Options.css "position" "absolute"
+        , Options.css "top" "0"
+        , Options.css "bottom" "0"
+        , Options.css "right" "0"
+        , Options.css "background" "black"
+        , Options.css "cursor" "col-resize"
+        , Events.on "mousedown" (Decode.map OnInventoryPanelResizeStart Mouse.position)
+            |> Options.attribute
+        ]
+        []
