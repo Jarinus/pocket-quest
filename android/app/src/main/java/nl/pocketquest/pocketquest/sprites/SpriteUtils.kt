fba5bd5f28a8721db014b093ef9c36ef0161c523
Thijs de Haan <thijsdehaan71@hotmail.com>
2017-11-02 16:07:04 +0100

    Things on multiple lines

--- android/app/src/main/java/nl/pocketquest/pocketquest/sprites/SpriteUtils.kt
+++ android/app/src/main/java/nl/pocketquest/pocketquest/sprites/SpriteUtils.kt
@@ -6,6 +6,15 @@ import nl.pocketquest.pocketquest.utils.Point
 import nl.pocketquest.pocketquest.utils.times
 
 
-fun Bitmap.padded(padding: Point) = Bitmap.createBitmap(
-        (width * padding.x).toInt(), (height * padding.y).toInt(), config
-).also { Canvas(it).drawBitmap(this, 0f, 0f, null) }
+fun Bitmap.padded(padding: Point) = createScaledEmptyBitmap(padding)
+        .also(this::drawInto)
+
+private fun Bitmap.drawInto(other: Bitmap) = Canvas(other)
+        .drawBitmap(this, 0f, 0f, null)
+
+private fun Bitmap.createScaledEmptyBitmap(padding: Point) = Bitmap.createBitmap(
+        (width * padding.x).toInt(),
+        (height * padding.y).toInt(),
+        config
+)
+
