02614e7e6d9639ef9b0d74f9b7c7571dfb9a99f8
Jari Germeraad <jarinoes@gmail.com>
2017-11-15 14:23:53 +0100

    ADD: Scheduling

---
+++ server/src/main/kotlin/nl/pocketquest/server/request/schedule/Scheduler.kt
@@ -0,0 +1,16 @@
+package nl.pocketquest.server.request.schedule
+
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.timerTask
+
+object Scheduler {
+    private val timer = Timer()
+
+    fun scheduleAfter(duration: Long, timeUnit: TimeUnit, task: () -> Unit) {
+        val timerTask = timerTask { task.invoke() }
+        val durationMillis = timeUnit.toMillis(duration)
+
+        timer.schedule(timerTask, durationMillis)
+    }
+}
