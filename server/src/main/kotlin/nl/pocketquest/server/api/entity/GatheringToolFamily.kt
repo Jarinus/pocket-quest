ad60fe222e9ad970ad8abd13271976e5d54a6466
TimArling <tim.arling.ta@gmail.com>
2018-02-06 15:10:38 +0100

    removed tostring from dataclass GatheringToolFamily

--- server/src/main/kotlin/nl/pocketquest/server/api/entity/GatheringToolFamily.kt
+++ server/src/main/kotlin/nl/pocketquest/server/api/entity/GatheringToolFamily.kt
@@ -3,11 +3,7 @@ package nl.pocketquest.server.api.entity
 data class GatheringToolFamily(
         val id: String,
         val members: Collection<String>
-) {
-    override fun toString(): String {
-        return "GatheringToolFamily(id='$id', members=$members)"
-    }
-}
+)
 
 data class GatheringToolFamilyModel(
 
